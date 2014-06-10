d99f86c449ad0b956f356fe5138ed0e4e0ae2126
David Frahm <david@midmoapps.com>
2014-06-09 21:02:19 -0500

    Adds sample app page; Changes cms module to properly return promises

--- src/cms.js
+++ src/cms.js
@@ -1,21 +1,35 @@
 "use strict";
 
+function stickIt(node) {
+
+}
+
 var cms = function () {
 
     var doSomething = function () {
         console.log('doSomething()');
     };
 
-    var getHtmlTextElement = function () {
-        var element = null;
-        $.ajax({url: 'http://cms.pwbly.com/object/i_echo_stuff'})
+    var getText = function () {
+        return $.ajax({url: 'http://cms.pwbly.com/object/i_echo_stuff'})
             .then(function (data) {
-                element = $('<div>' + data.content + '</div>');
+                console.log('then > data', data);
+                return data.content;
             })
             .fail(function () {
                 cms.log("API error on GET");
             });
-        return element;
+    };
+
+    var getHtmlTextElement = function () {
+        return getText()
+            .then(function (text) {
+                console.log('then > text', text);
+                return $('<p>' + text + '</p>');
+            })
+            .fail(function () {
+                cms.log("API error on GET");
+            });
     };
 
     var log = function (message) {
@@ -25,6 +39,7 @@ var cms = function () {
     return {
         doSomething: doSomething,
         html: {
+            getText: getText,
             getTextElement: getHtmlTextElement
         },
         log: log
--- test/spec/cmsModuleSpec.js
+++ test/spec/cmsModuleSpec.js
@@ -21,23 +21,36 @@ describe("Module", function () {
     });
 
     describe("HTML interface", function () {
-        describe("text object", function () {
-            it("should transform to a jQuery div node", function () {
+        describe("text", function () {
+            it("should return promise", function () {
                 var ajaxSpy = spyOn($, 'ajax').andCallFake(function () {
                     var d = $.Deferred();
-                    d.resolve({type: 'text', content: 'The content.'});
+                    d.resolve({type: "text", content: "The content."});
                     return d.promise();
                 });
 
-                var textElement = cms.html.getTextElement();
+                var promise = cms.html.getText();
 
                 expect(ajaxSpy).toHaveBeenCalled();
-                // TODO: Custom matcher(s), e.g., toBeJqueryElement() or toBeJqueryNode(), toHaveTagName('div')
-                expect(textElement).toEqual(jasmine.any(Object));
-                expect(textElement.size()).toEqual(1);
-                expect(textElement.html()).toEqual('The content.');
-                expect($("<div />").append(textElement.clone()).html()).toEqual('<div>The content.</div>');
-                expect(textElement.prop('tagName').toLowerCase()).toEqual('div');
+                // TODO: Custom matcher(s), e.g., toBeJqueryPromise()
+                expect(promise).toBeDefined();
+                expect(promise.promise).toEqual(jasmine.any(Function));
+            });
+            it("should resolve to string", function () {
+                var ajaxSpy = spyOn($, 'ajax').andCallFake(function () {
+                    var d = $.Deferred();
+                    d.resolve({type: "text", content: "The content."});
+                    return d.promise();
+                });
+
+                var text;
+                cms.html.getText()
+                    .then(function (returnText) {
+                        text = returnText;
+                    });
+
+                expect(ajaxSpy).toHaveBeenCalled();
+                expect(text).toEqual("The content.");
             });
             it("should throw error when ajax fails", function () {
                 var logSpy = spyOn(cms, 'log');
@@ -47,11 +60,61 @@ describe("Module", function () {
                     return d.promise();
                 });
 
-                var textElement = cms.html.getTextElement();
+                cms.html.getText();
 
                 expect(ajaxSpy).toHaveBeenCalled();
                 expect(logSpy).toHaveBeenCalled();
-                expect(textElement).toBeNull();
+            });
+        });
+
+        describe("text node", function () {
+            it("should return promise", function () {
+                var ajaxSpy = spyOn($, 'ajax').andCallFake(function () {
+                    var d = $.Deferred();
+                    d.resolve({type: "text", content: "The content."});
+                    return d.promise();
+                });
+
+                var promise = cms.html.getTextElement();
+
+                expect(ajaxSpy).toHaveBeenCalled();
+                // TODO: Custom matcher(s), e.g., toBeJqueryPromise()
+                expect(promise).toBeDefined();
+                expect(promise.promise).toEqual(jasmine.any(Function));
+            });
+            it("should transform to a jQuery p node", function () {
+                var ajaxSpy = spyOn($, 'ajax').andCallFake(function () {
+                    var d = $.Deferred();
+                    d.resolve({type: 'text', content: 'The content.'});
+                    return d.promise();
+                });
+
+                var textElement;
+                cms.html.getTextElement()
+                    .then(function (returnTextElement) {
+                        textElement = returnTextElement;
+                    });
+
+                expect(ajaxSpy).toHaveBeenCalled();
+                // TODO: Custom matcher(s), e.g., toBeJqueryElement() or toBeJqueryNode(), toHaveTagName('div')
+                expect(textElement).toEqual(jasmine.any(Object));
+                expect(textElement.size()).toEqual(1);
+                expect(textElement.html()).toEqual('The content.');
+                expect($("<div />").append(textElement.clone()).html()).toEqual('<p>The content.</p>');
+                expect(textElement.prop('tagName').toLowerCase()).toEqual('p');
+            });
+            it("should throw error when ajax fails", function () {
+                var logSpy = spyOn(cms, 'log');
+                var ajaxSpy = spyOn($, 'ajax').andCallFake(function () {
+                    var d = $.Deferred();
+                    d.reject({error: 'Some error message'});
+                    return d.promise();
+                });
+
+                cms.html.getTextElement();
+
+                expect(ajaxSpy).toHaveBeenCalled();
+                expect(logSpy).toHaveBeenCalled();
             });
         });
     });
